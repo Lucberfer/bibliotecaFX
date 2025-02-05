efbb15e7707ab112cf860aa9c1108ce9dba48331
Lucas Bernabe Fernandez <lucberfer@gmail.com>
2025-02-05 20:31:11 +0100

    go home

--- src/main/java/org/example/bibliotecafx/dao/AutorDAO.java
+++ src/main/java/org/example/bibliotecafx/dao/AutorDAO.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class AutorDAO {
@@ -21,6 +23,49 @@ public class AutorDAO {
             e.printStackTrace();
 
 
+        }
+    }
+
+    public void updateAutor(Autor autor) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(autor);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteAutor(Autor autor) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(autor);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public Autor getAutorById(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Autor.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Autor> getAllAutores() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Autor> query = session.createQuery("FROM Autor", Autor.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
         }
     }
 }
